198c4e484ffa6a2fb4e8564c1a02ffb693bf0f47
César Jeanroy <cesar.jeanroy@gmail.com>
2015-08-28 00:48:51 -0400

    Mobile navigation has been added

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 $(document).ready(function(){
   headerEffect();
   smoothNavigation();
+  showMobileNavigation();
 });
 
 
@@ -21,10 +22,23 @@ function headerEffect(){
     $el.waypoint( function( direction ) {
 
       if( direction === 'down' && animClassDown ) {
-        $head.attr('class', 'pf-header ' + animClassDown);        
+        //If it's the mobile version the target is the trigger button, not the header
+        if($(window).width()>480){
+          $head.attr('class', 'pf-header ' + animClassDown); 
+        }
+        else if( animClassDown !== "pf-header-hide"){
+          $(".pf-mobile-navigation-trigger").fadeIn(300);          
+        }              
       }
       else if( direction === 'up' && animClassUp ){
-        $head.attr('class', 'pf-header ' + animClassUp);
+        //If it's the mobile version the target is the trigger button, not the header
+        if($(window).width()>480){
+           $head.attr('class', 'pf-header ' + animClassUp); 
+        }
+        else{
+          $(".pf-mobile-navigation-trigger").fadeOut(300);
+        } 
+
       }
 
     }, { offset: '100%' } );
@@ -35,7 +49,32 @@ function headerEffect(){
  * Function that initialise the smoothNavigation
  */
 function smoothNavigation(){
-  $("a").on('click',function(){
+  $("a").on('click',function(ev){
+    ev.preventDefault();
+
+    //When a link is clicked from the mobile navigation
+    if($(window).width()<=480){
+      $(".pf-mobile-navigation-trigger").find("span").first().removeClass("is-clicked");
+      $("#pf-header").switchClass("pf-header-show","pf-header-hide");
+    }
+
     $(window).scrollTo($($(this).attr("href")),1000);
+    
+  });
+}
+
+function showMobileNavigation(){
+  $(".pf-mobile-navigation-trigger").on('click',function(){
+    
+    if($("#pf-header").hasClass("pf-header-hide")){
+      $(this).find("span").first().addClass("is-clicked");
+      $("#pf-header").switchClass("pf-header-hide","pf-header-show");
+
+    }
+    else{
+      $(this).find("span").first().removeClass("is-clicked");
+      $("#pf-header").switchClass("pf-header-show","pf-header-hide");
+    }
+    
   });
 }
