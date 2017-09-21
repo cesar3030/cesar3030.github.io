eb5c4bd01eeb36fc7a7dc0cfe8de8ceb06f3db9f
Cesar Jeanroy <cesar.jeanroy@gmail.com>
2017-09-21 09:09:17 -0400

    Updated prompt texe

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ var terminalContent=[
 },
 {
   type: "answer",
-  content: "Hello, I’m César, a software engineering student & full stack developer with a huge interest in web technologies.<br>When I’m not working or training with my triathlon club, you can catch me at a software meetup in San Francsico discussing anything and everything from software development to team management. Programming is more than a job for me, it’s a passion. I’m studying software engineering to find simple solutions to complex problems.",
+  content: "Hello, I’m César, a software engineering student & full stack developer with a huge interest in web technologies.<br>When I’m not working or training with my triathlon club, you can catch me at a software meetup in Montreal discussing anything and everything from software development to team management. Programming is more than a job for me, it’s a passion. I’m studying software engineering to find simple solutions to complex problems.",
   timeout:1
 },
 {
